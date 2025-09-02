a7b40febd54ac3662b82b0be4db125b1dda3c986
Renold-tech <renold111@gmail.com>
2025-09-03 00:54:50 +0530

    Understanding middlewares

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -3,7 +3,8 @@ export const MovieIndex = (req, res) => {
 };
 
 export const MovieCreate = (req, res) => {
-  res.send("Create a movie");
+  console.log(req.body);
+  return res.json(req.body);
 };
 
 export const MovieUpdate = (req, res) => {
--- main.js
+++ main.js
@@ -5,6 +5,9 @@ import connectDB from "./lib/db.js";
 const app = express();
 const PORT = 6969;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 connectDB();
 
 app.get("/", (req, res) => {
