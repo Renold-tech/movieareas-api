b9c8adad7aeaeb2436dd6dd030baead1c3960c03
Renold-tech <renold111@gmail.com>
2025-09-02 20:14:41 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,15 @@ app.get("/", (req, res) => {
   res.json({ msg: "Hello all" });
 });
 
+//CRUD functionlity of movies
+app.get("/movies", () => {});
+
+app.post("./movies", () => {});
+
+app.put("./movies/:id", () => {});
+
+app.delete("./movies/:id", () => {});
+
 app.listen(6969, () => {
   console.log(`The server is running at http://localhost:${PORT}`);
 });
