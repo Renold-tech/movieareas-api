b9faa35aec6bb5cb0bc6cdaa559100252e5f20b4
Renold-tech <renold111@gmail.com>
2025-09-03 09:32:02 +0530

    Add model

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -1,10 +1,22 @@
+import Movie from "../models/movie.model.js";
+
 export const MovieIndex = (req, res) => {
   res.send("Get all movie lists");
 };
 
-export const MovieCreate = (req, res) => {
+export const MovieCreate = async (req, res) => {
   console.log(req.body);
-  return res.json(req.body);
+
+  const newMovie = new Movie({
+    title: req.body.title,
+    desc: req.body.desc,
+  });
+  try {
+    const movie = await newMovie.save();
+    return res.status(201).json(movie);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 
 export const MovieUpdate = (req, res) => {
